73b094f47b7ee76ca06139964fb2a4d1226eef72
slomusti <musti@wlan-si.net>
2017-12-22 20:50:37 +0100

    functional

--- scripts/run-experiment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-work_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-echo "Running all KORUZA experiment monitoring scripts."
-nohup python ${work_dir}/link-test.py > /data/koruza-link-test.log &
-nohup python ${work_dir}/weather.py > /data/koruza-weather.log &
-wait
