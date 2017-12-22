9b4d8e105fe32bea0f3c80277920ec28d09e4440
slomusti <musti@wlan-si.net>
2017-12-22 20:32:07 +0100

    initial

--- scripts/link-test.py
+++ scripts/link-test.py
@@ -1,21 +1,3 @@
-'''
-KORUZA experiment v2 - batch measurement download
-Copyright (C) 2017  Institute IRNAS Rače <info@irnas.eu>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
 #!/usr/bin/env python
 from __future__ import print_function
 
@@ -32,6 +14,8 @@ RTT_REGEXP = re.compile(r'rtt min/avg/max/mdev = (.+?)/(.+?)/(.+?)/(.+?) ms')
 
 
 def push(uuid, link_id, sent, received, rtt_min, rtt_avg, rtt_max):
+    print('Link {} : {}%'.format(link_id,100.0 * (1.0 - float(received) / float(sent))))
+
     """Submit data to nodewatcher instance."""
     body = {
         'sensors.generic': {
