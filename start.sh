5fd765883e62dbe7e4bb95a9b17fe60a62819b2b
slomusti <musti@wlan-si.net>
2017-12-23 18:22:24 +0100

    fixed logging, not working well yet

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # Start the main application.
 echo "Running all KORUZA experiment monitoring scripts."
-nohup python ./scripts/weather.py & > /data/koruza-experiment.log &
-nohup python ./scripts/link-test.py & > /data/koruza-experiment.log &
+nohup python ./scripts/weather.py > /data/koruza-experiment.log &
+nohup python ./scripts/link-test.py > /data/koruza-experiment.log &
 tail -f /data/koruza-experiment.log
 wait
